4938d3bcf0af57f51ec2783dd1937aafad4e0691
Justin Singh-Mohudpur <justin.singh-mohudpur@noaa.gov>
2025-01-22 09:59:07 -0800

    pkg(typing): impl Provider protcol

--- src/bear/typing/__init__.py
+++ src/bear/typing/__init__.py
@@ -1,8 +1,24 @@
 from __future__ import annotations
 
-from typing import TypeVar, Generic, Callable
+from typing import (
+    Callable,
+    Generator,
+    Generic,
+    Optional,
+    Protocol,
+    TypeVar,
+    runtime_checkable,
+)
+
+from polars import LazyFrame
+from pyarrow import RecordBatch as ArrowRecordBatch
+from pyarrow import Schema as ArrowSchema
+
+from bear.core.fips import USCounty
 
 T = TypeVar("T")
+
+ArrowBatchGenerator = Generator[ArrowRecordBatch, None, None]
 
 
 class staticproperty(Generic[T]):
@@ -15,3 +31,59 @@ class staticproperty(Generic[T]):
     @staticmethod
     def __call__(getter_fn: Callable[[], T]) -> staticproperty[T]:
         return staticproperty(getter_fn)
+
+
+@runtime_checkable
+class Provider(Protocol):
+    """Provider Protocol
+
+    Provides an interface for functions a module should implement
+    to be considered a Provider.
+    """
+
+    @staticmethod
+    def epsg() -> int: ...
+
+    @staticmethod
+    def base_schema() -> Optional[ArrowSchema]: ...
+
+    @staticmethod
+    def read(county: USCounty, *args, **kwargs) -> ArrowBatchGenerator:
+        """Read provider data via a pyarrow.RecordBatch generator
+
+        Parameters
+        ----------
+        county : USCounty
+            The area of interest (AOI) to pull record batches from.
+        *args
+            Positional arguments passed to implementation.
+        **kwargs
+            Keyword arguments passed to implementation.
+
+        Returns
+        -------
+        ArrowBatchGenerator
+            A generator yielding pyarrow.RecordBatch objects.
+        """
+        ...
+
+    @staticmethod
+    def conform(lf: LazyFrame, *args, **kwargs) -> LazyFrame:
+        """Conform function definition
+
+        Parameters
+        ----------
+        lf : polars.LazyFrame
+            Deferred evaluation polars data frame to apply
+            the implemented conform function against.
+        *args
+            Positional arguments passed to implementation.
+        **kwargs
+            Keyword arguments passed to implementation.
+
+        Returns
+        -------
+        polars.LazyFrame
+            A new lazy frame with the conform expressions applied.
+        """
+        ...
